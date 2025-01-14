b353d00c45fcf4f07a374606116ceb357b9fa7c1
BeatrizArespacochaga <beatrizarespacochaga94@gmail.com>
2025-01-14 16:40:14 +0100

    made toggle menu

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,4 @@
+/*
 // Aquí puedes agregar funcionalidades adicionales, como la validación de formularios,
 // animaciones, o interacciones de la página.
 console.log("Página cargada correctamente");
@@ -5,4 +6,16 @@ console.log("Página cargada correctamente");
 // Ejemplo de animación para el botón de RSVP
 document.querySelector('.btn-rsvp').addEventListener('click', function() {
     alert("¡Gracias por confirmar tu asistencia!");
+}); 
+*/
+
+console.log("Página cargada correctamente");
+
+// Selección de elementos
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('nav ul');
+
+// Evento para mostrar/ocultar el menú
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('active'); // Agrega/quita la clase 'active'
 });
