654629904b85fc39939b2f1443cbe4fb08a1fac2
BeatrizArespacochaga <beatrizarespacochaga94@gmail.com>
2025-01-18 20:22:17 +0100

    finished landing boda mobile

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -19,3 +19,20 @@ const menu = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('active'); // Agrega/quita la clase 'active'
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const logo = document.querySelector(".logo");
+    const header = document.querySelector("header.menu-toggle");
+    
+    window.addEventListener("scroll", () => {
+        if (window.scrollY > 50) { // Cambia '50' según cuánto scroll deseas que se necesite.
+            logo.classList.add("logo-visible");
+            logo.classList.remove("hidden-logo");
+            header.classList.add("scrolled"); // Para aplicar otros cambios al header.
+        } else {
+            logo.classList.remove("logo-visible");
+            logo.classList.add("hidden-logo");
+            header.classList.remove("scrolled");
+        }
+    });
+});
